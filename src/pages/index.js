bae19257e010ae08e14923b6037d87e22e07a159
Vinicius Amaral <viniciuscopuesb@gmail.com>
2022-08-15 21:47:22 -0300

    feat: adding homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import {createGlobalStyle } from "styled-components"
 import Layout from '../components/layout';
 import '../styles/index.css';
+import DetalhePag from "../components/detalhePag";
+import Teste1 from "../components/teste1";
+import Teste2 from "../components/teste2";
+import ExperimentosIndex from "../components/experimentosIndex";
+import Colaboracao from "../assets/colaboracao.png";
+import Multilinguagem from "../assets/multilinguagem.png";
+import Contato from "../assets/contato.png";
 
 function Index() {
   return (
     <>
     <GlobalStyle/>
     <Layout>
-     <p>oiiiiiiiiiiiiii</p>
+    <DetalhePag />
+      <ExperimentosIndex />
+      <Teste1 comDiv={true} id="secao3" titulo="Colaboração" container="container sobre-colaboracao" desc="desc-colaboracao" conteudo="O LARA permite que os alunos possam resolver problemas e programar em equipes de modo que todos consigam interagir entre si e ver as mudanças que os colegas estão fazendo no trabalho, além de permitir que os alunos possam pedir ajuda a monitores e professores." image="img-colab" srcImage={Colaboracao} /> 
+      <Teste2 id="secao4" titulo="Multilinguagem" container="container sobre-multilinguagem" image="img-mult" srcImage={Multilinguagem} conteudo="Atualmente, os experimentos do LARA podem ser programados em C++ e Python para melhor satisfazer professores e alunos. E futuramente pretendemos abranger muitas outras linguagens de programação  e outras formas de programação como em bloco."/>
+      <Teste1 id="secao5" titulo="Entre em contato" container="container sobre-contato" desc="desc-contato" conteudo="Gostou do nosso projeto e gostaria de entrar em contato? Nos envie um email para o endereço lara@uesb.edu.br" image="img-contact" srcImage={Contato} />
       </Layout>
     </>
     
